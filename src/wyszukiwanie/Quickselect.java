8a2bfeddced927ad8991077930d4ae48a4032edc
Michal Fukowski <michal01f@gmail.com>
2026-08-10 16:24:57 +0200

    update quickselect to use recursion and lecture partition

--- src/wyszukiwanie/Quickselect.java
+++ src/wyszukiwanie/Quickselect.java
@@ -17,35 +17,47 @@ public class Quickselect {
      * @param arr tablica liczb całkowitych
      * @param k   pozycja szukanego elementu
      * @return wartość k-tego najmniejszego elementu w tablicy
-     * @throws IllegalStateException jeśli algorytm zakończy działanie bez znalezienia elementu
      */
 
     private static int kthSmallest(int[] arr, int k) {
-        k--; // dopasowanie k-tego elementu do indeksowania tablicy
-
-        int leftIndex = 0;
-        int rightIndex = arr.length - 1;
-
-        while (leftIndex <= rightIndex) {
-            int i = partition(arr, leftIndex, rightIndex);
-
-            if (i == k) {
-                return arr[k];
-            } else if (i > k) {
-                rightIndex = i - 1;
-            } else {
-                leftIndex = i + 1;
-            }
+        if (arr == null || k < 1 || k > arr.length) {
+            throw new IllegalArgumentException("Pusta tablica lub nieprawidłowa wartość k");
         }
 
-        throw new IllegalStateException("Nie znaleziono k-tego najmniejszego elementu");
+        return quickselect(arr, 0, arr.length - 1, k - 1);
     }
 
     /**
-     * Algorytm pomocniczy partition
+     * Algorytm Hoare'a (quickselect) - pomocnicza metoda rekurencyjna
+     *
+     * @param arr        tablica liczb całkowitych
+     * @param leftIndex  początkowy indeks przeszukiwanego fragmentu tablicy
+     * @param rightIndex końcowy indeks przeszukiwanego fragmentu tablicy
+     * @param k          indeks (tablicowy) szukanego elementu
+     * @return wartość szukanego k-tego najmniejszego elementu
+     */
+
+    private static int quickselect(int[] arr, int leftIndex, int rightIndex, int k) {
+        if (leftIndex == rightIndex) {
+            return arr[leftIndex];
+        }
+
+        int pivotIndex = partition(arr, leftIndex, rightIndex);
+
+        if (pivotIndex == k) {
+            return arr[k];
+        } else if (pivotIndex > k) {
+            return quickselect(arr, leftIndex, pivotIndex - 1, k);
+        } else {
+            return quickselect(arr, pivotIndex + 1, rightIndex, k);
+        }
+    }
+
+    /**
+     * Metoda pomocnicza partition
      * <p>
      * Przestawia elementy podtablicy tak, że elementy na lewo od pivota są niewiększe,
-     * a na prawo niemniejsze
+     * a na prawo niemniejsze.
      * <p>
      * Złożoność optymistyczna: O(n)
      * Złożoność pesymistyczna: O(n)
@@ -63,28 +75,32 @@ public class Quickselect {
         int i = leftIndex + 1;
         int j = rightIndex;
 
-        while (true) {
-            while (i <= rightIndex && arr[i] <= pivot) {
+        do {
+            while (i < rightIndex && arr[i] <= pivot) {
                 i++;
             }
-            while (j >= leftIndex && arr[j] > pivot) {
+            while (j > i && arr[j] >= pivot) {
                 j--;
             }
 
-            if (i >= j) {
-                break;
+            if (i < j) {
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
             }
+        } while (i < j);
 
-            int tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
+        if (arr[i] > pivot) {
+            arr[leftIndex] = arr[i - 1];
+            arr[i - 1] = pivot;
+
+            return i - 1;
+        } else {
+            arr[leftIndex] = arr[i];
+            arr[i] = pivot;
+
+            return i;
         }
-
-        int tmp = arr[leftIndex];
-        arr[leftIndex] = arr[j];
-        arr[j] = tmp;
-
-        return j;
     }
 
     public static void main(String[] args) {
